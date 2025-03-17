6786ce21efaceab6f8c0dddd945b3ba8973b28b5
Marcos V. O. Assis <mvoassis@gmail.com>
2025-03-17 14:00:03 -0300

    Add files via upload

---
+++ parser_helper.py
@@ -0,0 +1,26 @@
+from llama_parse import LlamaParse
+from llama_index.core import SimpleDirectoryReader
+
+
+def le_arquivo(arquivo):
+    parser = LlamaParse(
+        api_key="llx-HD5nnuet0K0Ssmmj8Wwbx4P7G77Tkw09VO6Jsmx55lt2nMmt",
+        # can also be set in your env as LLAMA_CLOUD_API_KEY
+        result_type="markdown",  # "markdown" and "text" are available
+        num_workers=4,  # if multiple files passed, split in `num_workers` API calls
+        verbose=True,
+        language='pt'
+    )
+
+    file_extractor = {".pdf": parser}
+    documents = SimpleDirectoryReader(
+        "./arquivos", file_extractor=file_extractor
+    ).load_data()
+
+
+    # documents = parser.load_data(arquivo)
+    # print(document[0].text[:300])
+    # with open("./arquivos/ppc-liccomp-parsed.md", "w", encoding="utf-8") as arquivo:
+    #     # Escrevendo o conteúdo da variável documento no arquivo
+    #     arquivo.write(document[0].text)
+    return documents
